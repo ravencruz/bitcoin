04b559eeeaa847b563a2a5feefd797dccac0e660
jcruz <sebastian.cruz@bibamgroup.com>
2020-09-30 20:51:50 -0300

    feat: dummy get price

--- src/main/kotlin/org/ground/play/bit/coin/controller/CoinController.kt
+++ src/main/kotlin/org/ground/play/bit/coin/controller/CoinController.kt
@@ -2,9 +2,11 @@ package org.ground.play.bit.coin.controller
 
 
 import org.ground.play.bit.coin.dto.Bitcoin
+import org.ground.play.bit.coin.model.BitcoinPrice
 import org.ground.play.bit.coin.service.CoinService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -14,8 +16,26 @@ class CoinController {
     @Autowired
     lateinit var service: CoinService
 
-    @GetMapping("/coin")
-    suspend fun coin() : Bitcoin {
-        return service.execute()
+    @GetMapping("/coin/price/current")
+    suspend fun currentPrice() : Bitcoin {
+        return service.currentPrice()
+    }
+
+    @GetMapping("/coin/price")
+    suspend fun saveSearch(@RequestParam time: String) : Bitcoin {
+        println("time $time")
+        return service.saveAndFind(time)
+    }
+
+    @GetMapping("/coin/find")
+    suspend fun price2(@RequestParam time: String) : BitcoinPrice {
+        println("time $time")
+        return service.findPrice(time)
+    }
+
+    @GetMapping("/coin/find/average")
+    suspend fun priceAverage(@RequestParam time1: String, @RequestParam time2: String) : Bitcoin {
+
+        return service.findAveragePrice(time1, time2)
     }
 }
--- src/main/kotlin/org/ground/play/bit/coin/service/CoinService.kt
+++ src/main/kotlin/org/ground/play/bit/coin/service/CoinService.kt
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.reactive.function.client.awaitExchange
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 
 @Service
 class CoinService {
@@ -26,12 +29,55 @@ class CoinService {
             .build()
 
     //TODO should return the DAO or a representation?
-    suspend fun execute(): Bitcoin {
+    suspend fun currentPrice(): Bitcoin {
         val bitcoinResponse = getPojoFromRequest()
         println("Bitcoin: $bitcoinResponse")
 
         bitcoinRepository.save(bitcoinResponse)
+        //TODO forma mas copada de hacer esto
         return Bitcoin(bitcoinResponse.lprice, bitcoinResponse.curr1, bitcoinResponse.curr2)
+    }
+
+    suspend fun saveAndFind(time: String): Bitcoin {
+        val dummyBitcoin = BitcoinPrice("0", "0", "0",  LocalDateTime.now())
+        println("Bitcoin: $dummyBitcoin")
+
+        val bitcoinResponse = getPojoFromRequest()
+        println("Bitcoin to save: $bitcoinResponse")
+        bitcoinRepository.save(bitcoinResponse)
+
+        val findByCreatedDate = bitcoinRepository.findByCreatedDate(bitcoinResponse.createdDate)
+        println("found: $findByCreatedDate")
+
+        return Bitcoin(dummyBitcoin.lprice, dummyBitcoin.curr1, dummyBitcoin.curr2)
+    }
+
+    suspend fun findPrice(time: String): BitcoinPrice {
+        val dummyBitcoin = BitcoinPrice("0", "0", "0",  LocalDateTime.now())
+        println("Bitcoin: $dummyBitcoin")
+
+
+        val localDate = LocalDateTime.parse(time, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+
+        val findByCreatedDate = bitcoinRepository.findByCreatedDate(localDate)
+        println("found: $findByCreatedDate")
+
+        return findByCreatedDate.get(0)
+    }
+
+    suspend fun findAveragePrice(startTime: String, endTime: String): Bitcoin {
+        val dummyBitcoin = BitcoinPrice("0", "0", "0",  LocalDateTime.now())
+        println("Bitcoin: $dummyBitcoin")
+
+
+        val localStartDate = LocalDateTime.parse(startTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        val localEndDate = LocalDateTime.parse(endTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+
+        val findByCreatedDate = bitcoinRepository.findByCreatedDateTimeBetween(localStartDate, localEndDate)
+        println("found range: $findByCreatedDate")
+
+
+        return Bitcoin(dummyBitcoin.lprice, dummyBitcoin.curr1, dummyBitcoin.curr2)
     }
 
     private suspend fun getPojoFromRequest(): BitcoinPrice {
